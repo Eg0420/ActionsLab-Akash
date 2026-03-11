67ec5233442ee8e3abcef00328d9f756747056e1
Eg0420 <akashmahadevan.chulliparambil@dcmail.ca>
2026-03-11 14:43:09 -0400

    Updated Calculator file

--- Calculator.java
+++ Calculator.java
@@ -7,5 +7,8 @@ public class Calculator {
     public int subtract(int a, int b){
         return a - b;
     }
- System.out.println("Calculator ready!");
+    
+     public static void main(String[] args){
+        System.out.println("Calculator ready!");
+    }
 }
